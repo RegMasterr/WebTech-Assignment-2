4a141f89e81d6c8d6f64ee78e739de4f27bbab55
Piee <105191365@student.swin.edu.au>
2025-05-20 13:52:19 +1000

    fixing minor errors in connection & testing connection to jobs table with simple code.

--- Part2/jobs.php
+++ Part2/jobs.php
@@ -1,11 +1,34 @@
 <?php
     require_once 'settings.php';
-    $db_conn = mysqli_connect($host, $user, $pwd, $database);
-    if ($db_conn) {
+    if ($conn) {
         $query = "SELECT * FROM jobs";
-        $result = mysqli_query($db_conn, $query);
-        mysqli_close($db_conn);
+        $result = mysqli_query($conn, $query);
+        mysqli_close($conn);
     } else {
         echo "<p> Connection failed. </p> " ;
     }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jobs</title>
+</head>
+<body>
+   <?php include 'inc_files/header.inc'; ?>
+    <?php
+            while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>" . $row['job_title'] . "</td>";
+            echo "<td>" . $row['job_description'] . "</td>";
+            echo "<td>" . $row['salary_range'] . "</td>";
+            echo "<td>" . $row['reports_to'] . "</td>";
+            echo "<td>" . $row['responsibilities'] . "</td>";
+            echo "<td>" . $row['qualifications'] . "</td>";
+            echo "</tr>";
+        }
+        ?>
+   <?php include 'inc_files/footer.inc'; ?>
+</body>
+</html>
