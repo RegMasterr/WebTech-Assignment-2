16b092a5cf38b12c652887d20f03da8625cd92be
Piee <105191365@student.swin.edu.au>
2025-05-22 11:19:13 +1000

    add CSS files to project folder

--- Part2/jobs.php
+++ Part2/jobs.php
@@ -13,6 +13,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jobs</title>
+    <link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
    <?php include 'inc_files/header.inc'; ?>
